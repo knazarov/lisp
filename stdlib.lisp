9a0e5e98f7311ac2608cb4ad04f1521c38234e93
Konstantin Nazarov <mail@racktear.com>
2020-05-12 23:34:52 +0300

    Initial implementation of a standard library

---
+++ stdlib.lisp
@@ -0,0 +1,30 @@
+(define list (lambda x x))
+(define cadr (lambda (x) (car (cdr x))))
+
+(defmacro defun params
+  (list 'define
+        (car params)
+        (cons 'lambda (cons
+                       (car (cdr params))
+                       (cdr (cdr params))
+                       )
+              )
+        )
+  )
+
+
+(defun map (fun list)
+  (if list
+      (cons (fun (car list)) (map fun (cdr list)))
+    )
+  )
+
+(defmacro let params
+  (cons (cons 'lambda
+              (cons
+               (map (lambda (x) (car x)) (car params))
+               (cdr params)
+               ))
+        (map (lambda (x) (cadr x)) (car params))
+        )
+  )
